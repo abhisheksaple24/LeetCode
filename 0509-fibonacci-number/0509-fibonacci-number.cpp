9b4047f201920d4022deb3a56113d2db38593a6f
Abhishek Saple <abhishek.saple24@pccoepune.org>
2026-07-14 23:33:04 +0530

    Time: 10 ms (46.58%), Space: 7.9 MB (38.32%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        // 0 1 1 2 3 5 8 13
+
+        if(n == 0 || n == 1) {
+            return n;
+        }
+
+        return fib(n-1) + fib(n-2);
+    }
+};
